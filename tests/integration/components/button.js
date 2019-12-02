1cb604a0fbc5e703c941fe5acf4013ffeb27ecc0
Patrick Pircher <patrick.pircher@ibm.com>
2019-12-02 13:30:05 +0100

    fix tests

---
+++ tests/integration/components/button.js
@@ -0,0 +1,18 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | Button', (hooks) => {
+  setupRenderingTest(hooks);
+
+  test('should set as primary', async function (assert) {
+    assert.expect(1);
+
+    this.set('isPrimary', 'true');
+
+    await render(hbs`<Button @primary={{this.isPrimary}} />`);
+
+    assert.ok(this.element.querySelector('div').getAttribute('class').includes('primary'));
+  });
+});
