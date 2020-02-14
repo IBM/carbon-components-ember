bacf77dc42948665cfba48cdb4398c83ef56e428
patricklx <patricklx@users.noreply.github.com>
2020-02-14 14:18:18 +0100

    Update render-svg-part.js

--- addon/components/icon/render-svg-part.js
+++ addon/components/icon/render-svg-part.js
@@ -1,9 +1,30 @@
 import { helper as buildHelper } from '@ember/component/helper';
 import { htmlSafe } from '@ember/template';
 
-export function renderSvgPart([svg]) {
-  const attrs = Object.keys(svg.attrs).map(a => `${a}="${svg.attrs[a]}"`).join(' ');
-  return htmlSafe(`<${svg.elem} ${attrs}>`);
+const cache = new Map();
+
+export function renderSvgPart([svg], { class: classes, fill }) {
+  if (!svg) return '';
+  const base = `<svg class="${classes.join(' ')}"
+             focusable="false"
+             fill="${fill}"
+             style="will-change: transform;"
+             width="${svg.attrs.width}"
+             height="${svg.attrs.height}"
+             viewBox="${svg.attrs.viewBox}">`
+  let rest = '';
+  if (cache.has(svg)) {
+    rest = cache.get(svg);
+  } else {
+    const part = svg.content.map((svgPart) => {
+      const attrs = Object.keys(svgPart.attrs).map(a => `${a}="${svgPart.attrs[a]}"`).join(' ');
+      return `<${svgPart.elem} ${attrs} />`;
+    }).join();
+    rest = part;
+    cache.set(svg, rest);
+  }
+  const html = (base + rest + '</svg>').trim();
+  return htmlSafe(html);
 }
 
 export const helper = buildHelper(renderSvgPart);
