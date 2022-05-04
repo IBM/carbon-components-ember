6cfaadd4100a19d33562f5bfc124b69c56a93788
patricklx <patricklx@users.noreply.github.com>
2022-05-04 16:07:18 +0200

    use npm

--- config/ember-try.js
+++ config/ember-try.js
@@ -10,7 +10,6 @@ module.exports = function() {
     getChannelURL('canary')
   ]).then((urls) => {
     return {
-      useYarn: true,
       scenarios: [
         {
           name: 'ember-lts-3.4',
