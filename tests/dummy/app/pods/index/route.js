07bb90cb2f233584ccef647c9ab488f642673345
patrick <pa.trickjlp@gmail.com>
2022-08-02 10:07:58 +0200

    cleanup

--- tests/dummy/app/pods/index/route.js
+++ tests/dummy/app/pods/index/route.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 class IndexRoute extends Route {
   redirect() {
-    this.replaceWith('docs.getting-started');
+    this.replaceWith('docs.getting-started.installation');
   }
 
 }
