561c8e3956f07738166d224483518a906d115bf0
Patrick Pircher <patrick.pircher@ibm.com>
2024-07-13 13:30:27 +0200

    fix ci
    Signed-off-by: Patrick Pircher <patrick.pircher@ibm.com>

--- test-app/app/app.js
+++ test-app/app/app.js
@@ -4,8 +4,6 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'test-app/config/environment';
 
-
-
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
