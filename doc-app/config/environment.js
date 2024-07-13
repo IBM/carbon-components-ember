5a11d1dd8d5bd4f4927a1f9bb706de933fd529ac
Patrick Pircher <patrick.pircher@ibm.com>
2024-07-13 22:18:08 +0200

    fix
    Signed-off-by: Patrick Pircher <patrick.pircher@ibm.com>

--- doc-app/config/environment.js
+++ doc-app/config/environment.js
@@ -42,7 +42,7 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
-    ENV.rootURL = '/carbon-components-ember';
+    ENV.rootURL = 'ADDON_DOCS_ROOT_URL';
     // here you can enable a production-specific feature
   }
 
