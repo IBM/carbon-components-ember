c0e8dbb2ca9e4dbc491854393bba4ae8801a4c9e
Patrick Pircher <patricklx@users.noreply.github.com>
2025-07-08 11:07:17 +0000

    docs

---
+++ versions/main/assets/index-Bpb4rIoI.js
@@ -0,0 +1,13 @@
+import { S as Selected, D as DocsService, k as Compiler, l as DocsService$1 } from './main-3DAl6isn.js';
+export { A as APIDocs, m as CommentQuery, v as Compiled, n as ComponentSignature, p as HelperSignature, M as ModifierSignature, q as addRoutes, w as getIndexPage, x as isCollection, y as isIndex } from './main-3DAl6isn.js';
+
+function registry(prefix) {
+  return {
+    [`${prefix}/services/kolay/api-docs`]: DocsService$1,
+    [`${prefix}/services/kolay/compiler`]: Compiler,
+    [`${prefix}/services/kolay/docs`]: DocsService,
+    [`${prefix}/services/kolay/selected`]: Selected
+  };
+}
+
+export { registry };
