8a4c6ef65d294a9630e80c62751feddbc71d4fa1
Patrick Pircher <patricklx@users.noreply.github.com>
2025-10-24 14:23:20 +0000

    docs

---
+++ versions/main/assets/index-DyJKjogG.js
@@ -0,0 +1,60 @@
+import { z as isArray$1, B as truthConvert } from './main-BkvZhrTA.js';
+export { E as and, F as eq, G as isEmpty, I as isEqual, K as not, J as notEq, O as or } from './main-BkvZhrTA.js';
+
+function gt(left, right, options) {
+  if (options?.forceNumber) {
+    if (typeof left !== 'number') {
+      left = Number(left);
+    }
+    if (typeof right !== 'number') {
+      right = Number(right);
+    }
+  }
+  return left > right;
+}
+
+function gte(left, right, options) {
+  if (options?.forceNumber) {
+    if (typeof left !== 'number') {
+      left = Number(left);
+    }
+    if (typeof right !== 'number') {
+      right = Number(right);
+    }
+  }
+  return left >= right;
+}
+
+function isArray(...params) {
+  return params.every(isArray$1);
+}
+
+function lt(left, right, options) {
+  if (options?.forceNumber) {
+    if (typeof left !== 'number') {
+      left = Number(left);
+    }
+    if (typeof right !== 'number') {
+      right = Number(right);
+    }
+  }
+  return left < right;
+}
+
+function lte(left, right, options) {
+  if (options?.forceNumber) {
+    if (typeof left !== 'number') {
+      left = Number(left);
+    }
+    if (typeof right !== 'number') {
+      right = Number(right);
+    }
+  }
+  return left <= right;
+}
+
+function xor(left, right) {
+  return truthConvert(left) !== truthConvert(right);
+}
+
+export { gt, gte, isArray, lt, lte, xor };
