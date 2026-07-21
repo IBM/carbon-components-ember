077f6818809cfa049e2a3b00642e31e06afd60c4
Patrick Pircher <patricklx@users.noreply.github.com>
2026-07-21 20:13:15 +0000

    Remove docs preview for PR #654

--- pr-previews/pr-654/assets/api-docs_virtual-D-SvMq8C.js
+++
@@ -1,9 +0,0 @@
-const packageNames = [
-  'carbon-components-ember',
-];
-
-const loadApiDocs = {
-  'carbon-components-ember': () => fetch('/carbon-components-ember/pr-previews/pr-654/docs/carbon-components-ember.json'),
-};
-
-export { loadApiDocs, packageNames };
