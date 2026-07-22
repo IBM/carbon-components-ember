52ba2848745c5152dd4b50fcabfad2b6d221c194
Patrick Pircher <patricklx@users.noreply.github.com>
2026-07-22 06:37:44 +0000

    Remove docs preview for PR #653

--- pr-previews/pr-653/assets/api-docs_virtual-Enmq-FcG.js
+++
@@ -1,9 +0,0 @@
-const packageNames = [
-  'carbon-components-ember',
-];
-
-const loadApiDocs = {
-  'carbon-components-ember': () => fetch('/carbon-components-ember/pr-previews/pr-653/docs/carbon-components-ember.json'),
-};
-
-export { loadApiDocs, packageNames };
