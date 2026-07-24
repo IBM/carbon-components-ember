3cfc1768c917d02ebbfeb03a83ffaa36d26b2234
Patrick Pircher <patricklx@users.noreply.github.com>
2026-07-24 12:42:55 +0000

    Remove docs preview for PR #659

--- pr-previews/pr-659/assets/api-docs_virtual-VCHZQm2P.js
+++
@@ -1,9 +0,0 @@
-const packageNames = [
-  'carbon-components-ember',
-];
-
-const loadApiDocs = {
-  'carbon-components-ember': () => fetch('/carbon-components-ember/pr-previews/pr-659/docs/carbon-components-ember.json'),
-};
-
-export { loadApiDocs, packageNames };
