50a9eedb1f6597f11de618106637cc91c397b835
Patrick Pircher <patricklx@users.noreply.github.com>
2026-07-30 20:08:51 +0000

    Deploy docs preview for PR #688 (69cb9ac209700b43295e0edb57c112391374b83a)

---
+++ pr-previews/pr-688/assets/internals-DfMMBsVx.js
@@ -0,0 +1,9 @@
+import { y as getCachedValueFor, z as guidFor } from './main-C2KSrfgt.js';
+
+const internals = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
+	__proto__: null,
+	cacheFor: getCachedValueFor,
+	guidFor
+}, Symbol.toStringTag, { value: 'Module' }));
+
+export { internals as i };
