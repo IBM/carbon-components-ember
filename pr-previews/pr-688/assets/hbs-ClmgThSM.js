9e051f7ca1c79a26e0b28a23c1d520f4917d3780
Patrick Pircher <patricklx@users.noreply.github.com>
2026-07-31 16:41:48 +0000

    Deploy docs preview for PR #688 (5704396a3efd2b1f47b23f981a77d5ceec6cc2d4)

---
+++ pr-previews/pr-688/assets/hbs-ClmgThSM.js
@@ -0,0 +1,88 @@
+import { U as isRecord } from './main-BBIjcnFw.js';
+import { m as makeOwner } from './owner-BJSdV7_6.js';
+
+let elementId = 0;
+
+/**
+ * @param {unknown} [ options ]
+ * @returns {{ scope: Record<string, unknown> }}
+ */
+function filterOptions(options) {
+  if (!isRecord(options)) {
+    return {
+      scope: {}
+    };
+  }
+  return {
+    scope: (/** @type {Record<string, unknown>}*/options?.scope || {})
+  };
+}
+
+/**
+ * @type {import('../../types.ts').CompilerConfig['compiler']}
+ */
+async function compiler(config, api) {
+  /**
+   * @type {import('../../types.ts').Compiler}
+   */
+  const hbsCompiler = {
+    compile: async (text, options) => {
+      const {
+        template
+      } = await api.tryResolve('@ember/template-compiler/runtime');
+      const component = template(text, {
+        scope: () => ({
+          ...filterOptions(config).scope,
+          ...filterOptions(options).scope
+        })
+      });
+
+      /**
+       * Some versions of ember implement the runtime template compiler incorrectly (albeit, correct enough for the constraints at the time).
+       * So we need to wait longer than a microtask queue request could take.
+       *
+       * To make sure that the template is compiled, and "component"
+       * has a value.
+       *
+       * See:
+       * - https://github.com/emberjs/ember.js/issues/20913
+       * - https://github.com/emberjs/ember.js/issues/20914
+       */
+      await new Promise(requestAnimationFrame);
+
+      /**
+       * Is this allowed here? or do I just return text,
+       * and do the above in 'render'
+       */
+      return component;
+    },
+    render: async (element, compiled, extra, compiler) => {
+      /**
+       *
+       * TODO: These will make things easier:
+       *    https://github.com/emberjs/rfcs/pull/1099
+       *    https://github.com/ember-cli/ember-addon-blueprint/blob/main/files/tests/test-helper.js
+       */
+      const attribute = `data-repl-sdk-ember-hbs-${elementId++}`;
+      element.setAttribute(attribute, '');
+      const {
+        renderComponent
+      } = await compiler.tryResolve('@ember/renderer');
+      const owner = makeOwner(config.owner);
+      const args = /** @type {Record<string, unknown> | undefined} */
+      extra && typeof extra === 'object' && 'args' in extra ? /** @type {Record<string, unknown>} */extra.args : undefined;
+      const result = renderComponent(compiled, {
+        into: element,
+        owner,
+        ...(args ? {
+          args
+        } : {})
+      });
+      compiler.announce('info', 'Ember Island Rendered');
+      return () => result.destroy();
+    }
+  };
+  return hbsCompiler;
+}
+
+export { compiler };
