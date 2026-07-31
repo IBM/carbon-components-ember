94d3dc0a07409f0158b9e3b4364e8735d7aea691
Patrick Pircher <patricklx@users.noreply.github.com>
2026-07-31 07:02:38 +0000

    Deploy docs preview for PR #688 (de6d720f98f36edf32129d58b28fec709766736c)

---
+++ pr-previews/pr-688/assets/globals-DDpBLH2d.js
@@ -0,0 +1,4 @@
+import { T as runtime } from './main-CALt1W3s.js';
+import { g as globalId } from './global-id-BrhFC90G-wPkaDz-T.js';
+
+globalThis[globalId] = runtime;
---
+++ pr-previews/pr-688/assets/index-COQQemPP.js
@@ -0,0 +1,60 @@
+import { X as isArray$1, Y as truthConvert } from './main-CALt1W3s.js';
+export { Z as and, $ as eq, a0 as isEmpty, a1 as isEqual, a2 as not, a3 as notEq, a4 as or } from './main-CALt1W3s.js';
+
+function gt(left, right, options) {
+  if (options?.forceNumber) {
+    if (typeof left !== 'number') {
+      left = Number(left);
+    }
+    if (typeof right !== 'number') {
+      right = Number(right);
+    }
+  }
+  return left > right;
+}
+
+function gte(left, right, options) {
+  if (options?.forceNumber) {
+    if (typeof left !== 'number') {
+      left = Number(left);
+    }
+    if (typeof right !== 'number') {
+      right = Number(right);
+    }
+  }
+  return left >= right;
+}
+
+function isArray(...params) {
+  return params.every(isArray$1);
+}
+
+function lt(left, right, options) {
+  if (options?.forceNumber) {
+    if (typeof left !== 'number') {
+      left = Number(left);
+    }
+    if (typeof right !== 'number') {
+      right = Number(right);
+    }
+  }
+  return left < right;
+}
+
+function lte(left, right, options) {
+  if (options?.forceNumber) {
+    if (typeof left !== 'number') {
+      left = Number(left);
+    }
+    if (typeof right !== 'number') {
+      right = Number(right);
+    }
+  }
+  return left <= right;
+}
+
+function xor(left, right) {
+  return truthConvert(left) !== truthConvert(right);
+}
+
+export { gt, gte, isArray, lt, lte, xor };
