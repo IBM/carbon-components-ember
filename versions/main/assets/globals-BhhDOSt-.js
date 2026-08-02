a247825289d361efb12a31c123b1754fe3bee774
Patrick Pircher <patricklx@users.noreply.github.com>
2026-08-02 13:16:20 +0000

    docs

---
+++ versions/main/assets/globals-BhhDOSt-.js
@@ -0,0 +1,4 @@
+import { T as runtime } from './main-ja8P-WQO.js';
+import { g as globalId } from './global-id-BrhFC90G-wPkaDz-T.js';
+
+globalThis[globalId] = runtime;
---
+++ versions/main/assets/internals-DdbYaeIr.js
@@ -0,0 +1,9 @@
+import { q as getCachedValueFor, v as guidFor } from './main-ja8P-WQO.js';
+
+const internals = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
+	__proto__: null,
+	cacheFor: getCachedValueFor,
+	guidFor
+}, Symbol.toStringTag, { value: 'Module' }));
+
+export { internals as i };
