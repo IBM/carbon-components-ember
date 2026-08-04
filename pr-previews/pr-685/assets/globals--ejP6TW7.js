0e3d219f15e2a2fd7eb4285ed25f211664763728
Patrick Pircher <patricklx@users.noreply.github.com>
2026-08-04 09:14:25 +0000

    Remove docs preview for PR #685

--- pr-previews/pr-685/assets/globals--ejP6TW7.js
+++
@@ -1,4 +0,0 @@
-import { T as runtime } from './main-CDit4Kyz.js';
-import { g as globalId } from './global-id-BrhFC90G-wPkaDz-T.js';
-
-globalThis[globalId] = runtime;
--- pr-previews/pr-685/assets/globals-C1bptaha.js
+++
@@ -1,4 +0,0 @@
-import { T as runtime } from './main-B13N1EyK.js';
-import { g as globalId } from './global-id-BrhFC90G-wPkaDz-T.js';
-
-globalThis[globalId] = runtime;
