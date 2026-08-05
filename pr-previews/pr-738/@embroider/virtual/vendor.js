b99f6e923be975cb965165a190891c1db662b9e1
Patrick Pircher <patricklx@users.noreply.github.com>
2026-08-05 06:23:30 +0000

    Deploy docs preview for PR #738 (aad1ae0f77680eedff022f97405bb8044e045a00)

---
+++ pr-previews/pr-738/@embroider/virtual/vendor.js
@@ -0,0 +1,8 @@
+window.EmberENV={ ...(window.EmberENV || {}), ...{
+  "EXTEND_PROTOTYPES": false,
+  "FEATURES": {},
+  "_APPLICATION_TEMPLATE_WRAPPER": false,
+  "_DEFAULT_ASYNC_OBSERVERS": true,
+  "_JQUERY_INTEGRATION": false,
+  "_TEMPLATE_ONLY_GLIMMER_COMPONENTS": true
+} };var runningTests=false;
