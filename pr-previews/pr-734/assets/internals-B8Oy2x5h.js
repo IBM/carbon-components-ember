760285833295aafb6c2063a55c3b65d91fa1c73c
Patrick Pircher <patricklx@users.noreply.github.com>
2026-08-05 07:41:50 +0000

    Remove docs preview for PR #734

--- pr-previews/pr-734/assets/internals-B8Oy2x5h.js
+++
@@ -1,9 +0,0 @@
-import { q as getCachedValueFor, v as guidFor } from './main-DJjZLVvH.js';
-
-const internals = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
-	__proto__: null,
-	cacheFor: getCachedValueFor,
-	guidFor
-}, Symbol.toStringTag, { value: 'Module' }));
-
-export { internals as i };
