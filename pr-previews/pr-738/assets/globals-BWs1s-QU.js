5a49acc80e805c4c95c4bd15b547c7f8405d6a98
Patrick Pircher <patricklx@users.noreply.github.com>
2026-08-12 02:24:38 +0000

    Deploy docs preview for PR #738 (8a3f40f55cdc1a7fe1b117193fab2838e9ce409e)

---
+++ pr-previews/pr-738/assets/globals-BWs1s-QU.js
@@ -0,0 +1,4 @@
+import { T as runtime } from './main-CA22e_N2.js';
+import { g as globalId } from './global-id-BrhFC90G-wPkaDz-T.js';
+
+globalThis[globalId] = runtime;
---
+++ pr-previews/pr-738/assets/internals-dZ0Bzy1j.js
@@ -0,0 +1,9 @@
+import { q as getCachedValueFor, v as guidFor } from './main-CA22e_N2.js';
+
+const internals = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
+	__proto__: null,
+	cacheFor: getCachedValueFor,
+	guidFor
+}, Symbol.toStringTag, { value: 'Module' }));
+
+export { internals as i };
