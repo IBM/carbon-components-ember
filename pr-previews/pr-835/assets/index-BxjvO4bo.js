8d24b26a111f36b737397dee6916b0ea500e3b94
Patrick Pircher <patricklx@users.noreply.github.com>
2026-09-09 14:29:24 +0000

    Deploy docs preview for PR #835 (1ed4e555f34834a621327841590fbc93637e2ded)

---
+++ pr-previews/pr-835/assets/index-BxjvO4bo.js
@@ -0,0 +1,60 @@
+import { a_ as isArray$1, a$ as truthConvert } from './main-D4AIm-1r.js';
+export { b0 as and, b1 as eq, b2 as isEmpty, b3 as isEqual, ap as not, b4 as notEq, b5 as or } from './main-D4AIm-1r.js';
+
+function gt(left, right, options) {
+  if (options?.forceNumber) {
+    if (typeof left !== 'number') {
+      left = Number(left);
+    }
+    if (typeof right !== 'number') {
+      right = Number(right);
+    }
+  }
+  return left > right;
+}
+
+function gte(left, right, options) {
+  if (options?.forceNumber) {
+    if (typeof left !== 'number') {
+      left = Number(left);
+    }
+    if (typeof right !== 'number') {
+      right = Number(right);
+    }
+  }
+  return left >= right;
+}
+
+function isArray(...params) {
+  return params.every(isArray$1);
+}
+
+function lt(left, right, options) {
+  if (options?.forceNumber) {
+    if (typeof left !== 'number') {
+      left = Number(left);
+    }
+    if (typeof right !== 'number') {
+      right = Number(right);
+    }
+  }
+  return left < right;
+}
+
+function lte(left, right, options) {
+  if (options?.forceNumber) {
+    if (typeof left !== 'number') {
+      left = Number(left);
+    }
+    if (typeof right !== 'number') {
+      right = Number(right);
+    }
+  }
+  return left <= right;
+}
+
+function xor(left, right) {
+  return truthConvert(left) !== truthConvert(right);
+}
+
+export { gt, gte, isArray, lt, lte, xor };
