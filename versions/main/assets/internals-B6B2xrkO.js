43914b7006d62167c57a2194a76a2168f805ca5b
Patrick Pircher <patricklx@users.noreply.github.com>
2026-09-10 13:04:23 +0000

    docs

---
+++ versions/main/assets/internals-B6B2xrkO.js
@@ -0,0 +1,9 @@
+import { q as getCachedValueFor, v as guidFor } from './main-C8pf3y0a.js';
+
+const internals = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
+	__proto__: null,
+	cacheFor: getCachedValueFor,
+	guidFor
+}, Symbol.toStringTag, { value: 'Module' }));
+
+export { internals as i };
