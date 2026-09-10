15a1525d9f9e110f981d98ac32d97f8cac2c8d13
Patrick Pircher <patricklx@users.noreply.github.com>
2026-09-10 12:45:13 +0000

    Remove docs preview for PR #838

--- pr-previews/pr-838/assets/api-docs_virtual-B_G5h19d.js
+++
@@ -1,9 +0,0 @@
-const packageNames = [
-  'carbon-components-ember',
-];
-
-const loadApiDocs = {
-  'carbon-components-ember': () => fetch('/carbon-components-ember/pr-previews/pr-838/docs/carbon-components-ember.json'),
-};
-
-export { loadApiDocs, packageNames };
