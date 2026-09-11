51c49833394d3d97c092cd0da30f488a7a6fead0
Patrick Pircher <patricklx@users.noreply.github.com>
2026-09-11 07:21:01 +0000

    Deploy docs preview for PR #862 (9b0e31f6c8359da9d63746279bca533d78686b5e)

---
+++ pr-previews/pr-862/assets/index-AsTuoOPo.js
@@ -0,0 +1,60 @@
+import { bk as isArray$1, bl as truthConvert } from './main-rpgOYvkN.js';
+export { bm as and, bn as eq, bo as isEmpty, bp as isEqual, ap as not, bq as notEq, br as or } from './main-rpgOYvkN.js';
+
+function gt(left, right, options) {
+  if (options?.forceNumber) {
+    if (typeof left !== 'number') {
+      left = Number(left);
+    }
+    if (typeof right !== 'number') {
+      right = Number(right);
+    }
+  }
+  return left > right;
+}
+
+function gte(left, right, options) {
+  if (options?.forceNumber) {
+    if (typeof left !== 'number') {
+      left = Number(left);
+    }
+    if (typeof right !== 'number') {
+      right = Number(right);
+    }
+  }
+  return left >= right;
+}
+
+function isArray(...params) {
+  return params.every(isArray$1);
+}
+
+function lt(left, right, options) {
+  if (options?.forceNumber) {
+    if (typeof left !== 'number') {
+      left = Number(left);
+    }
+    if (typeof right !== 'number') {
+      right = Number(right);
+    }
+  }
+  return left < right;
+}
+
+function lte(left, right, options) {
+  if (options?.forceNumber) {
+    if (typeof left !== 'number') {
+      left = Number(left);
+    }
+    if (typeof right !== 'number') {
+      right = Number(right);
+    }
+  }
+  return left <= right;
+}
+
+function xor(left, right) {
+  return truthConvert(left) !== truthConvert(right);
+}
+
+export { gt, gte, isArray, lt, lte, xor };
