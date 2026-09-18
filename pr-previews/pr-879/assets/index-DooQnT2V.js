3566bda8cfe76032078da06eb0a1c823b9d14b93
Patrick Pircher <patricklx@users.noreply.github.com>
2026-09-18 10:58:25 +0000

    Deploy docs preview for PR #879 (fefdf77760e35e1e226ffa603b002e71f7219654)

---
+++ pr-previews/pr-879/assets/index-DooQnT2V.js
@@ -0,0 +1,60 @@
+import { bR as isArray$1, bS as truthConvert } from './main-ARMEkVMa.js';
+export { bT as and, aN as eq, bU as isEmpty, bV as isEqual, ap as not, bW as notEq, bX as or } from './main-ARMEkVMa.js';
+
+function gt(left, right, options) {
+  if (options?.forceNumber) {
+    if (typeof left !== 'number') {
+      left = Number(left);
+    }
+    if (typeof right !== 'number') {
+      right = Number(right);
+    }
+  }
+  return left > right;
+}
+
+function gte(left, right, options) {
+  if (options?.forceNumber) {
+    if (typeof left !== 'number') {
+      left = Number(left);
+    }
+    if (typeof right !== 'number') {
+      right = Number(right);
+    }
+  }
+  return left >= right;
+}
+
+function isArray(...params) {
+  return params.every(isArray$1);
+}
+
+function lt(left, right, options) {
+  if (options?.forceNumber) {
+    if (typeof left !== 'number') {
+      left = Number(left);
+    }
+    if (typeof right !== 'number') {
+      right = Number(right);
+    }
+  }
+  return left < right;
+}
+
+function lte(left, right, options) {
+  if (options?.forceNumber) {
+    if (typeof left !== 'number') {
+      left = Number(left);
+    }
+    if (typeof right !== 'number') {
+      right = Number(right);
+    }
+  }
+  return left <= right;
+}
+
+function xor(left, right) {
+  return truthConvert(left) !== truthConvert(right);
+}
+
+export { gt, gte, isArray, lt, lte, xor };
