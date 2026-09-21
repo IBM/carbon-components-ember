80dc5d6275bb6e60bfec47a7898c5b5118ea594c
Patrick Pircher <patricklx@users.noreply.github.com>
2026-09-21 06:37:20 +0000

    Deploy docs preview for PR #886 (25d122e4a7427eff55daf34a52732b4697260bd4)

--- pr-previews/pr-886/assets/internals-CCcnx4NS.js
+++
@@ -1,9 +0,0 @@
-import { q as getCachedValueFor, v as guidFor } from './main-DYoX-3_p.js';
-
-const internals = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
-	__proto__: null,
-	cacheFor: getCachedValueFor,
-	guidFor
-}, Symbol.toStringTag, { value: 'Module' }));
-
-export { internals as i };
